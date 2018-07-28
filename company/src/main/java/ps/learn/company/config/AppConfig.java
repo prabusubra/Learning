65d5d22c04a4c3f837e7cf9223ba5876f66261cd
prabusubra <prabusubra@192.168.43.183>
2018-07-28 13:39:18 +0530

    added

--- company/src/main/java/ps/learn/company/config/AppConfig.java
+++ company/src/main/java/ps/learn/company/config/AppConfig.java
@@ -1,8 +1,11 @@
 package ps.learn.company.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
 
 @Configuration
 public class AppConfig {
-
+	public MessageConverter customeconvertor() {
+		return new myConvertor();
+	}
 }
